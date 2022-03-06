79811b8229aecd509ca3d304cfc36e9222268e8f
frc <calvi.fra@gmail.com>
2022-03-07 00:31:39 +0100

    added carousel style

--- app/components/detail/detailController.js
+++ app/components/detail/detailController.js
@@ -1,4 +1,5 @@
 app.controller('detailController', ['$scope', '$routeParams', 'filmService', function($scope, $routeParams, filmService) {
     $scope.filmTitle = $routeParams.title;    
     $scope.filmSrc = filmService.getFilmSrc($scope.filmTitle);
+    console.log($routeParams)
 }])
