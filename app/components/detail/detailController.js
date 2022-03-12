5af93781bbba6474a758078c02b57541ee15dbaa
frc <calvi.fra@gmail.com>
2022-03-12 21:18:12 +0100

    media queries 1

--- app/components/detail/detailController.js
+++ app/components/detail/detailController.js
@@ -10,7 +10,7 @@ app.controller('detailController', ['$scope', '$routeParams', 'dataService', 'us
     };
 
     angular.element(document).ready(function() {
-        const detailCnt = angular.element(document.querySelector('.detail'));
-        detailCnt[0].style.backgroundImage = `linear-gradient(90deg,#0f171e 10%,rgba(15,23,30,.8) 40%,rgba(15,23,30,0)), url(${$scope.film.imageurl[0]})`;
+        const background = angular.element(document.querySelector('.background'));
+        background[0].style.backgroundImage = `linear-gradient(90deg,#0f171e 10%,rgba(15,23,30,.8) 60%,rgba(15,23,30,0)), url(${$scope.film.imageurl[0]})`;
     }); 
 }])
